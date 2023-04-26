c0b92ee9ee2d5025d9ac1850dc62e12be6e4642f
Suzan Goger <suzangoger@gmail.com>
2023-04-26 13:36:31 +0100

    Selenium classes added

--- src/test/java/Utilities/Base.java
+++ src/test/java/Utilities/Base.java
@@ -1,33 +1,41 @@
 package Utilities;
-
+import com.github.javafaker.Faker;
 import io.github.bonigarcia.wdm.WebDriverManager;
 import org.junit.After;
 import org.junit.Before;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
 
 import java.time.Duration;
 
 public class Base {
-
-
-    protected WebDriver driver;
+    protected WebDriver driver; // baska package lardan extende edilen class lar icin protected
+    protected Faker faker = new Faker();
 
     @Before
-    public void setup() {
+    public void setup(){
         WebDriverManager.chromedriver().setup();
         driver = new ChromeDriver();
         driver.manage().window().maximize();
         driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(5));
-
     }
 
     @After
-    public void teardown() {
-
-       // driver.quit();
-
+    public void teardown(){
+        //driver.quit();
     }
 
+    public void waitAndClick(WebDriver driver, WebElement element){
+        WebDriverWait wait = new WebDriverWait(driver, Duration.ofSeconds(10));
+        wait.until(ExpectedConditions.elementToBeClickable(element)).click();
+    }
 
+    public void waitForVisibility(WebDriver driver, WebElement element){
+        WebDriverWait wait = new WebDriverWait(driver, Duration.ofSeconds(10));
+        wait.until(ExpectedConditions.visibilityOf(element));
+
+    }
 }
