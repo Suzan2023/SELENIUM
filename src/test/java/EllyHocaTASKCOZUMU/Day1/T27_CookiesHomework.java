6e3d799257dc910178283b3839918d946e5dadb8
Suzan Goger <suzangoger@gmail.com>
2023-05-06 17:34:30 +0100

    Selenium classes added

---
+++ src/test/java/EllyHocaTASKCOZUMU/Day1/T27_CookiesHomework.java
@@ -0,0 +1,14 @@
+package EllyHocaTASKCOZUMU.Day1;
+
+import utilities.TestBase;
+
+public class T27_CookiesHomework extends TestBase {
+/*
+    Go to URL: http://facebook.com
+    getCookies,
+    addCookie,
+    deleteCookieNamed,
+    deleteAllCookies
+ */
+
+}
---
+++ src/test/java/EllyHocaTASKCOZUMU/Day1/T28_FileUploadHomework.java
@@ -0,0 +1,9 @@
+package EllyHocaTASKCOZUMU.Day1;
+
+import utilities.TestBase;
+
+public class T28_FileUploadHomework extends TestBase {
+
+    //Go to URL: https://www.monsterindia.com/seeker/registration
+    //Upload file.
+}
